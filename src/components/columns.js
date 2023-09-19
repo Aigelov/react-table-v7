aadc575a1455ddef5b2fcfab895ee2e7c1effe17
Galym Aigelov <aig@exante.eu>
2023-09-20 00:02:10 +0600

    React Basic Table. Columns. Mock Data

---
+++ src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+  {
+    Header: 'ID',
+    accessor: 'id',
+  },
+  {
+    Header: 'First Name',
+    accessor: 'firstName',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'dateOfBirth',
+  },
+  {
+    Header: 'Country',
+    accessor: 'country',
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone',
+  },
+];
